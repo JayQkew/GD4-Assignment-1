376253da3da332be0a61ce60b4d76a6a17b4277c
JayQkew <rayjebb@gmail.com>
2025-02-23 18:12:31 +0200

    centered parent

--- Slime Game/Assets/_Scripts/Slime/SoftBody.cs
+++ Slime Game/Assets/_Scripts/Slime/SoftBody.cs
@@ -8,10 +8,11 @@ public class SoftBody : MonoBehaviour
     [Range(3, 40)]public int numberOfNodes = 10;
     public int radius = 3;
     public float nodeRadius = 0.001f;
+    [SerializeField] private Transform nodeParent;
     
     [Header("Soft Body Qualities")] 
     [Range(0,1)] public float dampingRatio;
-    [Range(0.01,10)]public float frequency;
+    [Range(0.1f,10)]public float frequency;
     
     
     public Sprite nodeSprite;
@@ -36,21 +37,19 @@ public class SoftBody : MonoBehaviour
 
     private void Update()
     {
-        // UpdatePosition();
         ApplySoftBodyQualityes();
         UpdateMesh();
-        // ShapePolygonCollider();
+        UpdatePosition();
     }
 
     private void UpdatePosition()
     {
-        Vector3 totalPosition = Vector3.zero;
-        foreach (var node in nodes)
+        Vector3 centerPos = Vector3.zero;
+        foreach (GameObject n in nodes)
         {
-            totalPosition += node.transform.position;
+            centerPos += n.transform.position;
         }
-        
-        transform.position = totalPosition/nodes.Count;
+        transform.position = centerPos/nodes.Count;
     }
 
     private void CreateNodes()
@@ -61,14 +60,11 @@ public class SoftBody : MonoBehaviour
 
             Rigidbody2D rb = node.AddComponent<Rigidbody2D>();
             rb.freezeRotation = true;
-            
-            // SpriteRenderer sr = node.AddComponent<SpriteRenderer>();
-            // sr.sprite = nodeSprite;
 
             CircleCollider2D cc = node.AddComponent<CircleCollider2D>();
             cc.radius = nodeRadius;
             
-            node.transform.SetParent(transform);
+            node.transform.SetParent(nodeParent == null ? transform : nodeParent);
             
             nodes.Add(node);
         }
@@ -79,11 +75,11 @@ public class SoftBody : MonoBehaviour
         float equalAngles = 360/numberOfNodes;
         float startAngle = 90;
 
-        for (int n = 0; n < nodes.Count; n++)
+        foreach (var n in nodes)
         {
             float x = Mathf.Cos(startAngle * Mathf.Deg2Rad) * radius;
             float y = Mathf.Sin(startAngle * Mathf.Deg2Rad) * radius;
-            nodes[n].transform.position = new Vector3(x, y, 0);
+            n.transform.position = new Vector3(x, y, 0);
             
             startAngle += equalAngles;
         }
@@ -117,8 +113,17 @@ public class SoftBody : MonoBehaviour
     {
         _mesh = new Mesh();
         _mesh.name = "SoftBodyMesh";
-        _meshFilter = gameObject.AddComponent<MeshFilter>();
-        _meshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+        if (nodeParent == null)
+        {
+            _meshFilter = gameObject.AddComponent<MeshFilter>();
+            _meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        }
+        else
+        {
+            _meshFilter = nodeParent.gameObject.AddComponent<MeshFilter>();
+            _meshRenderer = nodeParent.gameObject.AddComponent<MeshRenderer>();
+        }
         
         _verts = new Vector3[numberOfNodes];
         _uvs = new Vector2[numberOfNodes];
@@ -127,7 +132,7 @@ public class SoftBody : MonoBehaviour
         // Set vertices and UVs
         for (int n = 0; n < numberOfNodes; n++)
         {
-            _verts[n] = nodes[n].transform.position;
+            _verts[n] = nodes[n].transform.localPosition;
             _uvs[n] = new Vector2(_verts[n].x / radius, _verts[n].y / radius);
         }
 
@@ -153,7 +158,7 @@ public class SoftBody : MonoBehaviour
         // Update vertices
         for (int n = 0; n < numberOfNodes; n++)
         {
-            _verts[n] = nodes[n].transform.position;
+            _verts[n] = nodes[n].transform.localPosition;
         }
 
         _mesh.vertices = _verts;
