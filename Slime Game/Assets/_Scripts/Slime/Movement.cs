651e2e55ffadbe7c42fa3a7b83e232ca5225139a
JayQkew <rayjebb@gmail.com>
2025-03-12 21:56:36 +0200

    reimplementing movement

--- Slime Game/Assets/_Scripts/Slime/Movement.cs
+++ Slime Game/Assets/_Scripts/Slime/Movement.cs
@@ -13,14 +13,15 @@ public class Movement : MonoBehaviour
     [Header("Movement Settings")] [SerializeField]
     private float _movementMultiplier;
 
+    [Header("Jump Settings")]
+    [SerializeField] private float _jumpMultiplier;
     [SerializeField] private bool _canJump;
     [SerializeField] private float displacementThreshold;
     [SerializeField] private float checkTime = 0.5f;
     private float currTime;
 
-    [Header("Spring Settings")] [SerializeField]
-    private float _maxFrequency;
-
+    [Header("Spring Settings")] 
+    [SerializeField] private float _maxFrequency;
     [SerializeField] private float _midFrequency;
     [SerializeField] private float _minFrequency;
 
@@ -39,6 +40,29 @@ public class Movement : MonoBehaviour
     {
         Jump(inputHandler.jumpInput);
         Grab(inputHandler.grabInput);
+        Move(inputHandler.aimInput);
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    private void Move(Vector2 dir)
+    {
+        int xInput = 0;
+        if(dir.x > 0) xInput = 1;
+        else if(dir.x < 0) xInput = -1;
+        Vector2 inputDir = new Vector2(xInput, 0);
+        
+        Debug.Log(inputDir);
+        
+        foreach (Rigidbody2D rb in _softBody.nodes_rb)
+        {
+            if (rb.transform.position.y >= _softBody.transform.position.y)
+            {
+                rb.AddForce( dir * (_movementMultiplier * 100 * Time.deltaTime), ForceMode2D.Force);
+            }
+        }
     }
 
     private void Jump(bool jumpInput)
@@ -57,8 +81,8 @@ public class Movement : MonoBehaviour
                 if (rb.bodyType == RigidbodyType2D.Dynamic)
                 {
                     if (rb.transform.position.y >= _softBody.transform.position.y)
-                        rb.AddForce(inputHandler.aimInput * _movementMultiplier, ForceMode2D.Impulse);
-                    else rb.AddForce(inputHandler.aimInput * (_movementMultiplier * 0.5f), ForceMode2D.Impulse);
+                        rb.AddForce(inputHandler.aimInput * _jumpMultiplier, ForceMode2D.Impulse);
+                    else rb.AddForce(inputHandler.aimInput * (_jumpMultiplier * 0.5f), ForceMode2D.Impulse);
                 }
 
                 rb.bodyType = RigidbodyType2D.Dynamic;
