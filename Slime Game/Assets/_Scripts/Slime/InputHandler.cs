c9e01d7958a6b9b923fe9b868e0c121da6faf0df
JayQkew <rayjebb@gmail.com>
2025-03-12 23:24:17 +0200

    remapping input action asset

--- Slime Game/Assets/_Scripts/Slime/InputHandler.cs
+++ Slime Game/Assets/_Scripts/Slime/InputHandler.cs
@@ -7,8 +7,6 @@ using UnityEngine.Serialization;
 public class InputHandler : MonoBehaviour
 {
     public PlayerInput playerInput;
-    public InputMode inputMode;
-    private GameObject softBody;
 
     public UnityEvent OnJumpPressed;
     public UnityEvent OnGrab;
@@ -16,28 +14,15 @@ public class InputHandler : MonoBehaviour
     
     [Header("Small Slime Movement")]
     public Vector2 aimInput;
-    public bool grabInput;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
-        inputMode = GetInputMode();
-        softBody = transform.GetChild(0).gameObject;
     }
     
-    public void Aim(InputAction.CallbackContext ctx)
+    public void Move(InputAction.CallbackContext ctx)
     {
-        if (inputMode == InputMode.KeyboardMouse)
-        {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(ctx.ReadValue<Vector2>());
-            Vector2 dir = mousePos - (Vector2)softBody.transform.position;
-            aimInput = new Vector2(dir.x, dir.y);
-        }
-        else
-        {
-            Vector2 dir = ctx.ReadValue<Vector2>();
-            aimInput = new Vector2(dir.x, dir.y);
-        }
+        aimInput = ctx.ReadValue<Vector2>();
     }
 
     public void Jump(InputAction.CallbackContext ctx)
@@ -58,15 +43,6 @@ public class InputHandler : MonoBehaviour
         {
             OnRelease?.Invoke();
         }
-    }
-    
-    private InputMode GetInputMode()
-    {
-        if (playerInput.currentControlScheme == "Gamepad") return InputMode.Gamepad;
-        if (playerInput.currentControlScheme == "Keyboard&Mouse") return InputMode.KeyboardMouse;
-        if (playerInput.currentControlScheme == "Joystick") return InputMode.Joystick;
-
-        return InputMode.KeyboardMouse;
     }
 
     public enum InputMode
