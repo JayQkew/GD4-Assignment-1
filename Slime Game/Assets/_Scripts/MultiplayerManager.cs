d649f6025a6fea58bc352188a5ba2bf1a4657fc7
JayQkew <rayjebb@gmail.com>
2025-03-17 13:59:57 +0200

    starting multiplayer lobby implementation

--- Slime Game/Assets/_Scripts/MultiplayerManager.cs
+++ Slime Game/Assets/_Scripts/MultiplayerManager.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInputManager))]
 public class MultiplayerManager : MonoBehaviour
 {
+    public static MultiplayerManager Instance {get; private set;}
     private PlayerInputManager _playerInputManager;
     public int playerCount = 0;
 
@@ -12,10 +15,26 @@ public class MultiplayerManager : MonoBehaviour
     [SerializeField] private Material[] _materials;
 
     [SerializeField] private Movement[] _playerMovements = new Movement[2];
+    public List<PlayerInput> players = new List<PlayerInput>();
     
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
         _playerInputManager = GetComponent<PlayerInputManager>();
+    }
+
+    public void RegisterPlayer(PlayerInput playerInput)
+    {
+        playerCount++;
+        players.Add(playerInput);
     }
     
     public void OnPlayerJoined(PlayerInput playerInput)
