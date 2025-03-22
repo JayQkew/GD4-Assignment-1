919ac9b4beed0c2739482c819115f93b3851733d
Michael-Naidoo <2583482@students.wits.ac.za>
2025-03-22 14:16:06 +0200

    added scene swapper
    ends the level after a player scores 4 goals (can be changed in editor)
    switches to another random scene that is not the current scene.

--- Slime Game/Assets/_Scripts/SceneChangeManager.cs
+++ Slime Game/Assets/_Scripts/SceneChangeManager.cs
@@ -4,10 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else if (Instance != this) Destroy(gameObject);
+    }
+    
+    public static SceneChangeManager Instance {get; private set;}
     public void ChangeScene()
     {
-        int newScene = UnityEngine.Random.Range(SceneManager.sceneCount - 1, 0);
-        LoadNewScene(newScene);
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        if (sceneCount <= 1) return; // Prevent errors if there is only one scene.
+
+        int newScene;
+        do
+        {
+            newScene = UnityEngine.Random.Range(0, sceneCount);
+        } while (newScene == SceneManager.GetActiveScene().buildIndex); // Ensure a different scene
+
+        SceneManager.LoadScene(newScene);
     }
 
     private void LoadNewScene(int newScene)
