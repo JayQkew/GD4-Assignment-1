abb7f984d538609e504ac54e1c337248edb12d86
JayQkew <rayjebb@gmail.com>
2025-06-05 23:11:26 +0200

    making map card a button

--- Slime Game/Assets/_Scripts/Maps/MapCard.cs
+++ Slime Game/Assets/_Scripts/Maps/MapCard.cs
@@ -1,12 +1,23 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapCard : MonoBehaviour
 {
     public Map map;
-    
+    private Button button;
+
+    private void Awake() {
+        button = GetComponent<Button>();
+    }
+
     public void SetMapCard(Map newMap) {
         map = newMap;
         GetComponentInChildren<TextMeshProUGUI>().text = newMap.mapName.Substring(4, newMap.mapName.Length - 4);
+        button.onClick.AddListener(ClickCard);
     }
+
+    private void ClickCard() => map.selected = !map.selected;
+    
 }
