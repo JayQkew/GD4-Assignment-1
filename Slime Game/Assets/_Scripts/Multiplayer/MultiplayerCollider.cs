20166b6b96cf42630a2a3bc76337c0e8a6a779e2
JayQkew <rayjebb@gmail.com>
2025-06-08 11:57:42 +0200

    Update MultiplayerCollider.cs
    fixed collider bug

--- Slime Game/Assets/_Scripts/Multiplayer/MultiplayerCollider.cs
+++ Slime Game/Assets/_Scripts/Multiplayer/MultiplayerCollider.cs
@@ -1,24 +1,26 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MultiplayerCollider : MonoBehaviour
 {
     private PlayerInputManager playerInputManager;
-    private Movement[] playerMovements = new Movement[2];
-    private PolygonCollider2D[] playerColliders = new PolygonCollider2D[2];
-    private SoftBody[] playerSoftBodies = new SoftBody[2];
+    [SerializeField] private Movement[] playerMovements = new Movement[2];
+    public PolygonCollider2D[] playerColliders = new PolygonCollider2D[2];
+    [SerializeField] private SoftBody[] playerSoftBodies = new SoftBody[2];
 
     private void Awake() => playerInputManager = GetComponent<PlayerInputManager>();
 
     private void Update() {
-        if (playerInputManager.playerCount == 2) PreventOverlap();
+        if (playerColliders[0] && playerColliders[1]) PreventOverlap();
     }
 
     public void OnPlayerJoined(PlayerInput playerInput) {
         playerMovements[playerInputManager.playerCount - 1] = playerInput.GetComponentInChildren<Movement>();
-        playerColliders[playerInputManager.playerCount - 1] = playerInput.GetComponentInChildren<PolygonCollider2D>();
         playerSoftBodies[playerInputManager.playerCount - 1] = playerInput.GetComponentInChildren<SoftBody>();
+        
+        StartCoroutine(GetCollider(playerInput, playerInputManager.playerCount - 1));
     }
 
     private void PreventOverlap() {
@@ -27,5 +29,10 @@ public class MultiplayerCollider : MonoBehaviour
             playerSoftBodies[0].AddForce(-distance.normal * Mathf.Abs(distance.distance));
             playerSoftBodies[1].AddForce(distance.normal * Mathf.Abs(distance.distance));
         }
+    }
+
+    private IEnumerator GetCollider(PlayerInput playerInput, int playerIndex) {
+        yield return null;
+        playerColliders[playerIndex] = playerInput.GetComponentInChildren<PolygonCollider2D>();
     }
 }
