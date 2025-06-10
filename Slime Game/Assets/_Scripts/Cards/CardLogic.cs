47dbfbdd29842043317137fbb4f87498c2005c14
JayQkew <rayjebb@gmail.com>
2025-06-10 23:08:20 +0200

    added basic card select

--- Slime Game/Assets/_Scripts/Cards/CardLogic.cs
+++ Slime Game/Assets/_Scripts/Cards/CardLogic.cs
@@ -1,18 +1,40 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CardLogic : MonoBehaviour
+public class CardLogic : MonoBehaviour,
+    ISelectHandler,
+    IDeselectHandler,
+    ISubmitHandler,
+    ICancelHandler
 {
     public Card card;
     [SerializeField] private TextMeshProUGUI header;
     [SerializeField] private TextMeshProUGUI description;
     [SerializeField] private Image image;
+
     public void SetCard(Card c) {
         card = c;
         //get ui and make it match
         header.text = c.cardName;
         description.text = c.description;
         image.sprite = c.icon;
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+    }
+
+    public void OnDeselect(BaseEventData eventData) {
+    }
+
+    public void OnSubmit(BaseEventData eventData) {
+        Debug.Log(card.cardName + " submitted");
+        //add card to players deck
+        GameManager.Instance.SwitchState(GameState.Round);
+    }
+
+    public void OnCancel(BaseEventData eventData) {
     }
 }
