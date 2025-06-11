5f955b374c0f3ffc643a40372247c544ee907b9b
JayQkew <rayjebb@gmail.com>
2025-06-11 22:41:45 +0200

    adding podium scene

---
+++ Slime Game/Assets/_Scripts/UI/PodiumScreen.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PodiumScreen : MonoBehaviour
+{
+    public void StartScreen() {
+        SceneManager.LoadScene("StartScreen");
+    }
+}
